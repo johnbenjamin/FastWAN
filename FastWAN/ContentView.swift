4bf8e98be496be84ea587f0bafc4d31e122cadb9
ZhiQiang Lin <zhiqiang.lin@compass.com>
2022-07-12 21:21:23 +0800

    Initial Commit

---
+++ FastWAN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FastWAN
+//
+//  Created by Xeon on 2022/7/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FastWAN/FastWANApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastWANApp.swift
+//  FastWAN
+//
+//  Created by Xeon on 2022/7/12.
+//
+
+import SwiftUI
+
+@main
+struct FastWANApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
